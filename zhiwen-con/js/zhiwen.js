d6329c0ecdbe4e219c4a2f96c00379737a8caeed
zjh <113196053@qq,com>
2017-06-28 21:06:26 +0800

    01

--- zhiwen-con/js/zhiwen.js
+++ zhiwen-con/js/zhiwen.js
@@ -1,3 +1,48 @@
+(function($){
+	var PageSwitch =(function (){
+		function  PageSwitch(element,options){
+			this.settins=$.extend(true,$.fn.PageSwitch.default,options||{});
+			this.element=element;
+			this.init();
+		}
+		PageSwitch.prototype={
+			init:function(){
+
+			}
+		}
+
+	});
+		$.fn.PageSwitch=function(options){
+			return this.each(function(){
+				var me=$(this),
+				instance=me.data("PageSwitch");
+				if(!instance){
+					instance=new PageSwitch(me,options);
+					me.data("PageSwitch",instance);
+				}
+				if($.type(options)==="string") return instance[options]();
+
+			});
+
+		}
+		$.fn.PageSwitch.default={
+			selectors:{
+				sections:".sections",
+				section:".section",
+				page:".pages",
+				active:".active"
+			},
+			index:0,
+			easing:"ease",
+			duration:500,
+			loop:false,
+			pagination:true,
+			keyboard:true,
+			direction:"vertial",
+			callback:""
+		}
+})(jQuery);
 $(function(){
-	$('input').button();
+	$(["data-PageSwitch"]).PageSwitch();
+
 });
